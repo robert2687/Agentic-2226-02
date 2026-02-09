90bd37c0eaa053e7da27a54910f18cf0ec98c57f
robert2687 <robertmalik26@gmail.com>
2026-02-09 15:37:45 +0100

    feat: add Tailwind CSS setup with configuration files and styles

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    content: [
+        './index.html',
+        './index.tsx',
+        './App.tsx',
+        './components/**/*.{ts,tsx}',
+        './hooks/**/*.{ts,tsx}',
+        './lib/**/*.{ts,tsx}',
+        './services/**/*.{ts,tsx}'
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: []
+};
